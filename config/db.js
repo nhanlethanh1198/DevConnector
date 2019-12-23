fc8e6c767bc5687cc29793310e6cafde0330c674
Le Nhan <nhanlethanh1198@gmail.com>
2019-12-23 23:14:38 +0700

    Repair deploy to Heroku [5]

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const config = require("config");
 require("dotenv").config();
 
-const db = process.env.LOCAL_URI_DEV || config.get("mongoURI");
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
 	try {

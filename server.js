1769c12eaccddea494e1884a0a94d8f880094ed7
Le Nhan <nhanlethanh1198@gmail.com>
2019-12-23 22:46:54 +0700

    Repair deploy to Heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 app.disable("x-powered-by");
@@ -8,27 +9,22 @@ app.disable("x-powered-by");
 app.use(express.json({ extended: false }));
 
 // Connect Database
-try {
-	connectDB();
-	app.get("/", (req, res) => {
-		try {
-			res.send("API Running");
-		} catch (error) {
-			res.json({
-				msg: error.response.statusText,
-				status: error.response.status
-			});
-		}
-	});
-} catch (error) {
-	console.log("Error while fetching from database!");
-}
+connectDB();
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static asset in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
